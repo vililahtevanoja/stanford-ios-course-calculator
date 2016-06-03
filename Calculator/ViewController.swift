6fd74b2a533107cdda178f889ec79dc795a5a4e9
vililahtevanoja <vili.lahtevanoja@gmail.com>
2016-06-03 11:31:51 +0300

    UI updates

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,15 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    private var userTypingInteger = true
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        let textCurrentlyInDisplay = display!.text!
+        if userIsInTheMiddleOfTyping {
+            if digit == "." && userTypingInteger  {
+                userTypingInteger = false
+                display!.text = textCurrentlyInDisplay + digit
+            }
+            else {
+                display!.text = textCurrentlyInDisplay + digit
+            }
+        } else {
+            display!.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+            userTypingInteger = true
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
     }
 }
-
